5f828624b5f55e862a61a16bd41f65e603cc1810
04231303 <bayramovmubariz@gmail.com>
2025-05-05 13:52:04 +0400

    docker-compose

--- springjava20/springjava20/src/main/java/az/developia/springjava20/service/BasketService.java
+++ springjava20/springjava20/src/main/java/az/developia/springjava20/service/BasketService.java
@@ -1,9 +1,12 @@
 package az.developia.springjava20.service;
 
+import org.springframework.stereotype.Service;
+
 import az.developia.springjava20.request.BookAddToBasketRequest;
 import az.developia.springjava20.response.CommonAddResponse;
 import jakarta.validation.Valid;
 
+@Service
 public class BasketService {
 
 	public CommonAddResponse add(@Valid BookAddToBasketRequest req) {

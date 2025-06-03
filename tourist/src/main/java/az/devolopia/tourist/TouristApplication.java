ef5fc8ba5a926dc2284a89e6537e093c48e5d04c
04231303 <bayramovmubariz@gmail.com>
2025-06-03 10:49:32 +0400

    Touris-start

---
+++ tourist/src/main/java/az/devolopia/tourist/TouristApplication.java
@@ -0,0 +1,13 @@
+package az.devolopia.tourist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristApplication.class, args);
+	}
+
+}
